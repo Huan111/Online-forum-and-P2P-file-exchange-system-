93f5014729a759efd1d74c24a6014c2514cec341
Huan111 <lgh1007115150@qq.com>
2021-03-29 03:30:26 +0800

    implement userlog

--- client.py
+++ client.py
@@ -5,10 +5,12 @@ import time
 #Define connection (socket) parameters
 #Address + Port no
 #Server would be running on the same host as Client
-
+if len(sys.argv) != 4:
+    raise ValueError('Invalid arguments. The terminal command format should meet: python3 client.py server_IP server_port client_udp_port.')
 serverName = sys.argv[1]
-
 serverPort = int(sys.argv[2])
+UDPport = sys.argv[3]
+
 clientSocket = socket(AF_INET, SOCK_STREAM)
 
 clientSocket.connect((serverName, serverPort))
@@ -16,7 +18,14 @@ message = ''
 connected = True
 while connected:
     data = clientSocket.recv(1024).decode()
-    if data[0] != 'F':
+    if data[0] != 'F' and data[0] != 'D' and data[0] != 'W':
+        message = input(data)
+        while message.strip() == '':
+            print('Input should not be empty. Please enter again.')
+            message = input(data)
+        clientSocket.send(message.encode())
+    elif data[0] == 'W':
+        clientSocket.send(UDPport.encode())
         message = input(data)
         clientSocket.send(message.encode())
     else:
--- server.py
+++ server.py
@@ -3,20 +3,24 @@ import sys
 import time
 import threading
 
+if len(sys.argv) != 3:
+    raise ValueError('Invalid arguments. The terminal command should meet the format: python3 server.py serverport number_of_consecutive_failed_attempts.')
 serverPort = int(sys.argv[1])
 TRY_count = sys.argv[2]
+DISCONNECT_MESSAGE = 'OUT'
+
 
 if float(TRY_count) != int(float(TRY_count)) or int(TRY_count)> 5:
     raise ValueError('The login trying number should be the integer between 1 and 5')
+TRY_count = int(TRY_count)
 
-TRY_count = int(sys.argv[2])
 #user login
-def login(connectionSocket): 
+def login(connectionSocket,address): 
     user_list = []
     passwd_list = []
     block_list = []
     try_count = 1
-
+    #Get the users and passwds
     with open('credentials.txt') as f:
         line = f.readline()
         while line:
@@ -24,7 +28,7 @@ def login(connectionSocket):
             user_list.append(user)
             passwd_list.append(passwd)
             line = f.readline()
-    
+    #Produce block list
     try:
         with open("block.txt", "r+") as f:
             d = f.readlines()
@@ -69,8 +73,23 @@ def login(connectionSocket):
                 connectionSocket.send('Failed.Your account is still in blocked, please try later.'.encode())
                 break
             else:
-                connectionSocket.send('Welcome to Toom!\n Enter one of the following commands (MSG, DLT, EDT, RDM, ATU, OUT):'.encode())
-                return True
+                connectionSocket.send(f'Welcome {user}!\nEnter one of the following commands (MSG, DLT, EDT, RDM, ATU, OUT):'.encode())
+                UDP_port = connectionSocket.recv(1024).decode()
+                t = time.ctime().split()
+                temp_t = t[2] + ' ' + t[1] + ' ' + t[-1] + ' ' + t[-2]
+                try:
+                    with open('userlog.txt') as f:
+                        d = f.readlines()
+                    idx = len(d) + 1
+                    with open('userlog.txt','a') as f:
+                        temp = str(idx) + '; ' + temp_t + '; ' + user + '; ' + address + '; ' + UDP_port + '\n'
+                        f.write(temp)
+                except:
+                    idx = 1
+                    with open("userlog.txt", "a") as f:
+                        temp = '1' + '; ' + temp_t + '; ' + user + '; ' + address + '; ' + UDP_port + '\n'
+                        f.write(temp)
+                return user,idx
         else:
             if try_count < TRY_count:
                 connectionSocket.send(f'User name or password is not correct. Please try again. Left trying time: {TRY_count - try_count}\n'.encode())
@@ -85,16 +104,49 @@ def login(connectionSocket):
                 break
     return False
 
-host = 'localhost'
+def logout(conn,user,idx):
+    with open('userlog.txt','r+') as f:
+        d = f.readlines()
+        f.seek(0)
+        for i in d:
+            temp_list = i.strip().split('; ')
+            if temp_list[2] != user:
+                if int(temp_list[0]) > idx:
+                    new_idx = str(int(temp_list[0]) - 1)
+                    temp_list[0] = new_idx
+                    i = '; '.join(temp_list) + '\n'
+                else:
+                    pass
+                f.write(i)
+        f.truncate()
+    conn.send(f'Successfully disconnted with the server. See ya {user}!'.encode())
+
+def handle_client(conn, addr):
+    user,idx = login(conn,addr[0])
+    if user:
+        print(f'[NEW CONNECTION] {addr} connected.')
+        connected = True
+    else:
+        connected = False
+    while connected:
+        msg = conn.recv(1024).decode()
+        if msg == DISCONNECT_MESSAGE:
+            connected = False
+            logout(conn,user,idx)
+    conn.close()
+
+SERVER = 'localhost'
 serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind((host,serverPort))
+serverSocket.bind((SERVER,serverPort))
+
 def start():
     serverSocket.listen()
     print('Server is listening...')
     while 1:
         conn, addr = serverSocket.accept()
-        thread = threading.Thread(target=login,args=[conn])
+        thread = threading.Thread(target=handle_client,args=[conn,addr])
         thread.start()
         print(f'[ACTIVE CONNECTIONS] {threading.active_count() - 1}')
+
 start()
         
